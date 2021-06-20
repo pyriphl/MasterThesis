e712ef6f2248076b25ca0c57c240c1afd49ee5f7
xinyi zhang <xinyi.zhang@hotmail.it>
2021-06-20 18:13:41 +0200

    #modified distance_models.py: complete the homologous seq calculation

--- distance_models.py
+++ distance_models.py
@@ -4,32 +4,47 @@ import random
 import numpy
 from Bio.Seq import Seq
 
-N = 1000  # length of the seq
-M = 2000  # random substitution freq 1/M
-R = 10  # number of experiments
-
-
+# for test:
+# calculation of genetic distance between two homologous sequences,
+# that is the number of of substitutions that have accumulated between them since they diverged from their common ancestor.
 def test():
-    result = [[]]
-    for t in range(1, R):
+    N = 1000  # length of the seq
+    M = 2000  # random substitution freq 1/M
+    R = 10  # number of experiments
+    result = [0] * M
+    mean_t = [0] * R
+    for t in range(0, R - 1):
         seq = gen_random_seq(N)
         seq1 = seq
-        t += 1
-        for i in range(1, M):
+        print(seq)
+        for i in range(0, M - 1):
             n = random.randint(0, N - 1)
-            seq1[n] = random.choice("CGTA")
-            result[i][t] = seq - seq1
-            mean = numpy.mean(result)
+            temp = list(str(seq1))
+            temp[n] = random.choice("CGTA")
+            old = str(seq)
+            new = str(seq1)
+            seq1 = Seq(''.join(temp))
+            difference = sum(1 for a, b in zip(old, new) if a != b)
+            result[i] = difference
+            i += 1
+        mean_t[t] = numpy.mean(result)
+        t += 1
+    mean = numpy.mean(result)
+    print(JC_distance(mean / N, N))
+    # print(JC_distance(90 / 948))
 
-def JC_distance(p):  # p = observed proportion
+
+def JC_distance(p, N) -> (float, float):  # p = observed proportion
+    if p >= 3 / 4:
+        print("p should be smaller than 3/4")
+        return -1, -1
     E = math.sqrt(p * (1 - p) / (N * ((1 - (4 / 3) * p) ** 2)))  # standard deviation
     d = -3 / 4 * math.log(1 - p * 4 / 3)  # distance
-    print(E)
-    print(d)
+    return d, E
+
 
 def gen_random_seq(length):
     letters = "CGTA"
     DNA = "".join(random.choices(letters, k=length))
     seq = Seq(DNA)
     return seq
-JC_distance(90/948)
--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from ete3 import Tree
 from Bio.Seq import Seq
+
+from distance_models import test
+
+
 def print_hi(seq):
     # Use a breakpoint in the code line below to debug your script.
     # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     my_seq = Seq(seq)
     print(my_seq)
+    test()
 
 
 # Press the green button in the gutter to run the script.
