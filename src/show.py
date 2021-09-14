018e75bd9198a62e43a80cea7d0f18848abe64ce
xinyi zhang <xinyi.zhang@hotmail.it>
2021-09-14 16:07:07 +0200

    C# show table

--- src/show.py
+++ src/show.py
@@ -1,6 +1,43 @@
 import matplotlib.pyplot as plt
-from matplotlib import cm
+from scipy.interpolate import griddata
+from tabulate import tabulate
 import numpy as np
+
+
+def show_table(gtr, tree, names):
+    print(gtr)
+    A = []
+    for i in range(len(names)):
+        line = [names[i]]
+        line.extend(tree[i])
+        A.append(line)
+    # A = np.vstack((names, tree))
+    print(tabulate(A, names))
+
+
+def get_points_values(table):
+    points = []
+    values = []
+    for i in range(0, len(table[0])):
+        for j in range(0, len(table[0])):
+            points.append((i, j))
+            values.append(table[i][j])
+    return points, values
+
+
+# model type : 'nearest', 'linear', 'cubic'
+def plot_graph(table, model):
+    grid_x, grid_y = np.mgrid[0:5:100j, 0:5:100j]
+    points, values = get_points_values(table)
+    # print(points)
+    # print((values))
+    print(grid_x)
+    grid_z = griddata(points, values, (grid_x, grid_y), method=model)
+    plt.subplot(221)
+    plt.imshow(grid_z.T, extent=(0, 1, 0, 1), origin='lower')
+    plt.title(model)
+    # plt.gcf().set_size_inches(6, 6)
+    plt.show()
 
 
 def plot_points(t1, t2):
@@ -21,8 +58,8 @@ def plot_points(t1, t2):
 
             t2_xs.append(i)
             t2_ys.append(j)
-            t2_zs.append(t1[i][j])
-            ax.scatter(t1_xs, t1_ys, t1_zs, marker='^')
+            t2_zs.append(t2[i][j])
+            ax.scatter(t2_xs, t2_ys, t2_zs, marker='^')
     ax.set_xlabel('taxa')
     ax.set_ylabel('taxa')
     ax.set_zlabel('distance')
